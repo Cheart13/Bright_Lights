7e8fa0ae293c95fc356ffaeb09e62ffa51911c75
cheart <cheart1308@gmail.com>
2022-02-03 21:12:25 +0200

    first

---
+++ js/code.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	infinite:true,
+	Arrows:false,
+	//prevArrow:<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="">Previous</button>,
+	//nextArrow:<button class="slick-next slick-arrow" aria-label="Next" type="button" style="">Next</button>,
+	
+})
+{/*<button type="button" class="slick-next"><img src="../resourses/arrow-left.svg" alt=''/></button>*/}
+console.log('NOde works');
